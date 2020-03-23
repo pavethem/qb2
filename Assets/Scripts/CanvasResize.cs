1d69955ed10962ee40205334b1be9d78db5ecfd7
pavethem <tino.helmig@gmx.de>
2020-03-23 17:02:51 +0100

    canvas resize adjustment for hd+ resolutions

--- Assets/Scripts/CanvasResize.cs
+++ Assets/Scripts/CanvasResize.cs
@@ -5,13 +5,15 @@ public class CanvasResize : MonoBehaviour {
     private bool readytoScale;
 
     protected void OnRectTransformDimensionsChange() {
+
+        float adjustment = 1 + (((Screen.currentResolution.width - 1920) / 1920) * 2);
         
         //resize canvas, but only up to a point (scalefactor of 0 means fullscreen)
-        if (Screen.width > 1100) {
+        if (Screen.width > 1100 * adjustment) {
             if(scalefactor != 0)
                 GetComponent<Canvas>().scaleFactor = scalefactor;
             else {
-                GetComponent<Canvas>().scaleFactor = 1.33f;
+                GetComponent<Canvas>().scaleFactor = 1.33f + adjustment - 1;
             }
         }
         else {
